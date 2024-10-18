f9ba2d283fdc7afcdf1b400f53938944815eaab1
郭夕 <guoxi@fadada.com>
2024-10-18 16:50:15 +0800

    feat: 构建进度条

--- packages/webpack/webpackConfig/webpack.base.js
+++ packages/webpack/webpackConfig/webpack.base.js
@@ -1,4 +1,33 @@
+const webpack = require("webpack");
+const cliProgress = require('cli-progress')
+
+const cliProgressBar = new cliProgress.Bar(
+  {
+    // clearOnComplete: true,
+    stopOnComplete: true,
+    linewrap: true,
+    hideCursor: true,
+  },
+  {
+    format:
+      "@kwok webpack progress [{bar}] {percentage}% | ETA: {eta}s | {text}",
+    barCompleteChar: "=",
+    barIncompleteChar: "-",
+  }
+);
+function handler(percentage, ...args) {
+  if (!cliProgressBar.isActive) {
+    cliProgressBar.start(1, 0);
+  }
+  cliProgressBar.update(percentage, { text: args.join(" ") });
+}
+
 module.exports = {
   mode: "development",
   devtool: "inline-source-map",
-}
+
+  plugins: [
+    // 进度
+    new webpack.ProgressPlugin(handler),
+  ],
+};
