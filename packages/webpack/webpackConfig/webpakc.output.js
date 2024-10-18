9fd301312bc0f466abdb3a266b39159901f20bb4
郭夕 <guoxi@fadada.com>
2024-10-18 15:54:25 +0800

    feat: 代码分离

--- packages/webpack/webpackConfig/webpakc.output.js
+++ packages/webpack/webpackConfig/webpakc.output.js
@@ -4,7 +4,7 @@ module.exports = {
   output: {
     clean: true,
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "../dist"),
     publicPath: "/",
   },
 };
